44388be2638a2d9256055035f1cb418bef072b9f
Umar Bilal <umar@dynamiconlinetechnologies.com>
2019-05-29 01:16:51 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'bill-calculator';
+  package_ = "Free";
+  cycle = "Monthly";
+  nusers = 1;
+  cost = 0;
+  total = 0;
+  discount = 0;
+  verdict = ""
+  cycleString = {
+    "Monthly": "month",
+    "Annual": "yearly",
+  };
+  packages = {
+    "Free": 0,
+    "Starter": 50,
+    "Premium": 100
+  };
+  discounts = {
+    "Annual": 0.2,
+    "Monthly": 0
+  };
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  calculateTotal() {
+    this.cost = this.packages[this.package_];
+    this.verdict = this.cycleString[this.cycle];
+    this.total = 0;
+    this.discount = 0;
+    if (this.cycle === "Annual") {
+      this.total = ((this.packages[this.package_] * 12) * this.nusers);
+      this.discount = this.total * this.discounts[this.cycle];
+      this.total = this.total - this.discount;
+    } else {
+      this.total = (this.packages[this.package_] * this.nusers)
+
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
