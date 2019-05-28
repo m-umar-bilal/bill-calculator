0c378b60e8c5e85c56287db03dd10f979603e4c7
Umar Bilal <umar@dynamiconlinetechnologies.com>
2019-05-29 01:19:07 +0500

    heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./bill-calculator/dist'));
+app.use(express.static('./dist/bill-calculator'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/bill-calculator/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/bill-calculator/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
